fef8990e4bf43a246ecedaf78e313d198fb0db2c
yygg0506 <xjunyan86@gmail.com>
2026-08-17 19:17:53 +0800

    [fix] lib_check_number_range: add max/min int check

--- tools/lib/check/lib_check_number_range.sh
+++ tools/lib/check/lib_check_number_range.sh
@@ -7,11 +7,13 @@ lib_check_number_range(){
     local max="$3"
 
     ### cehck ###
-    ! lib_check_number_int "$input" && return 1 #[bad] bad value
+    lib_check_number_int "$input" || return 1 #[bad] bad value
+    lib_check_number_int "$min" || return 2 #[bad] bad value
+    lib_check_number_int "$max" || return 3 #[bad] bad value
 
     ### main ###
-    [ -n "$min" ] && [ "$input" -lt "$min" ] && return 2 #[bad] too small
-    [ -n "$max" ] && [ "$input" -gt "$max" ] && return 3 #[bad] too large
+    [ -n "$min" ] && [ "$input" -lt "$min" ] && return 4 #[bad] too small
+    [ -n "$max" ] && [ "$input" -gt "$max" ] && return 5 #[bad] too large
     return 0
 }
 
@@ -20,8 +22,10 @@ lib_check_number_range_result(){
     local input="$1"
 
     case "$input" in
-        1) echo "[bad] bad value" ;;
-        2) echo "[bad] too small" ;;
-        3) echo "[bad] too large" ;;
+        1) echo "[bad] bad value (input)" ;;
+        2) echo "[bad] bad value (min)" ;;
+        3) echo "[bad] bad value (max)" ;;
+        4) echo "[bad] too small" ;;
+        5) echo "[bad] too large" ;;
     esac
 }

992ea31099482fbdb2484ce8fcea0892559b882c
yygg0506 <xjunyan86@gmail.com>
2026-08-18 15:31:39 +0800

    [fix] check_number_float: check multiple delimeter

--- tools/lib/check/lib_check_number_float.sh
+++ tools/lib/check/lib_check_number_float.sh
@@ -7,7 +7,7 @@ lib_check_number_float(){
     local debug=0
     while getopts "d" opt; do
         case "$opt" in
-            d) debug=1 ;;
+            d) debug=1; clear ;;
         esac
     done
 
@@ -29,6 +29,7 @@ lib_check_number_float(){
         #debug
         [ "$debug" -eq 1 ] && echo "[debug] lib_check_number_float: [ -n \$sym ]: yes" >&2
 
+        [ "${#sym}" -ne 1 ] && return 7 #[bad] check_number_float: (sym) bad input
         lib_check_number_int "$inte" || return 1 #[bad] check_number_float: (inte) bad input
         [ -n "$deci" ] || return 2 #[bad] check_number_float: (deci) no input
         [ "${deci:0:1}" = "-" ] && return 3 #[bad] check_number_float: (deci) not allow "-" for deci
@@ -42,7 +43,7 @@ lib_check_number_float(){
     fi
 
     #debug
-    [ "$debug" -eq 1 ] && echo "[debug] lib_check_number_float: good" >&2
+    [ "$debug" -eq 1 ] && echo "[debug] lib_check_number_float[0]: good" >&2
 
     return 0
 }
@@ -52,9 +53,10 @@ lib_check_number_float_result(){
     case "$input" in
         1) echo "[bad] check_number_float[1]: (inte) bad input" >&2 ;;
         2) echo "[bad] check_number_float[2]: (deci) no input" >&2 ;;
-        3) echo "[bad] check_number_float[3]: (deci) bad input" >&2 ;;
-        4) echo "[bad] check_number_float[4]: (deci) not allow "-" for deci" >&2 ;;
+        3) echo "[bad] check_number_float[3]: (deci) not allow "-" for deci" >&2 ;;
+        4) echo "[bad] check_number_float[4]: (deci) bad input" >&2 ;;
         5) echo "[bad] check_number_float[5]: (float) bad input, can't just input "-"" >&2 ;;
         6) echo "[bad] check_number_float[6]: (float) bad input, not valid number" >&2 ;;
+        7) echo "[bad] check_number_float[7]: (sym) bad input" >&2 ;;
     esac
 }
